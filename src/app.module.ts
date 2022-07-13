3e331ea747f1d2973ca1d9ba9b7fd5f9fef45763
Gabriel <gcrepho@gmail.com>
2022-07-13 14:49:47 -0300

    test nest generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
