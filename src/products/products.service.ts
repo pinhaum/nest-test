50cd2afe5339a491ba07f658c343ac5572f939aa
Gabriel <gcrepho@gmail.com>
2022-07-12 21:30:31 -0300

    add Delete Product route

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -7,7 +7,7 @@ export class ProductsService {
   private products: Product[] = [];
 
   insertProduct(title: string, desc: string, price: number) {
-    const prodId = new Date().toString();
+    const prodId = Math.random().toString();
     const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
     return prodId;
@@ -35,6 +35,11 @@ export class ProductsService {
       updatedProduct.price = price;
     }
     this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(prodId: string) {
+    const [product, index] = this.findProduct(prodId);
+    this.products.splice(index, 1);
   }
 
   private findProduct(id: string): [Product, number] {
